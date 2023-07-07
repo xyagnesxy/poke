07ba1aef8bcd9fbe4b0d46bed03120bb7baaae2e
Agnes Raimundo da Rocha <agnesrocha@intranet.tre-al.gov.br>
2023-07-07 12:22:19 -0300

    update

--- js/js.js
+++ js/js.js
@@ -1,8 +1,13 @@
-async function api(){
+async function pokemons(){
     const url = "../pokemons.json"
     const request = new Request(url)
     const resposta =  await fetch(request)
-    const lista = await resposta.json()
+    
+    const lista =  await resposta.json()
+    //console.log(lista)
+    return await lista//retorna uma promisse e não a lista
 }
-api()
+
+
+
 
